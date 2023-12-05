a5a1f8023a8d5fac944a4056829a1532e786bd6a
Lorenitz <lorena.g.saar@gmail.com>
2023-12-05 08:24:31 +0100

    adding session

--- chapter07/webapp/app.py
+++ chapter07/webapp/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, session
 import swimclub
 import os
 
 app = Flask(__name__)
+app.secret_key = "You will never guess..."
 
 #The "/swimmers"URL responds to a HTTP GET Request.
 @app.get("/swimmers")
@@ -11,22 +12,23 @@ app = Flask(__name__)
 def display_swimmers():
     swim_files = os.listdir(swimclub.FOLDER)
     swim_files.remove(".DS_Store")
-    swimmers = {}
+    
+    session["swimmers"] = {}
     
     for file in swim_files:
         name, *_  = swimclub.read_swim_data(file)
         #To remove the duplicated ones
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(file)
+        if name not in session["swimmers"]:
+            session["swimmers"][name] = []
+        session["swimmers"][name].append(file)
     #The result of this function are converted to a string 
     #before they are returned (just in case)    
-    return str(sorted(swimmers)) 
+    return str(sorted(session["swimmers"]))
 
 
-@app.get("/files/<swimmers>")
+@app.get("/files/<swimmer>")
 def get_swimmers_files(swimmer):
-    return str(swimmers[swimmer])    
+    return str(session["swimmers"][swimmer])
    
 app.run(debug=True)
         
