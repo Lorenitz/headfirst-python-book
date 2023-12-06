b8cd3d41228ea967db802d1712de9f3ce236ce77
Lorenitz <lorena.g.saar@gmail.com>
2023-12-06 11:21:59 +0100

    Adding functionality to not loose date

--- chapter07/webapp/app.py
+++ chapter07/webapp/app.py
@@ -5,29 +5,34 @@ import os
 app = Flask(__name__)
 app.secret_key = "You will never guess..."
 
+
+#This function doesn't return anything
+def populate_data():
+    #The if statements ensures the session is populated only when necessary
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        swim_files.remove(".DS_Store")
+        
+        session["swimmers"] = {}
+        
+        for file in swim_files:
+            name, *_ = swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
+            
+
 #The "/swimmers"URL responds to a HTTP GET Request.
 @app.get("/swimmers")
-
 #The function's signature.
 def display_swimmers():
-    swim_files = os.listdir(swimclub.FOLDER)
-    swim_files.remove(".DS_Store")
-    
-    session["swimmers"] = {}
-    
-    for file in swim_files:
-        name, *_  = swimclub.read_swim_data(file)
-        #To remove the duplicated ones
-        if name not in session["swimmers"]:
-            session["swimmers"][name] = []
-        session["swimmers"][name].append(file)
-    #The result of this function are converted to a string 
-    #before they are returned (just in case)    
+    populate_data()
     return str(sorted(session["swimmers"]))
 
 
 @app.get("/files/<swimmer>")
 def get_swimmers_files(swimmer):
+    populate_data()
     return str(session["swimmers"][swimmer])
    
 app.run(debug=True)
