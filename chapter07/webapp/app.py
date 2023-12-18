5c2c1e50d20e207e5c0722092bb6d6d148c4c39e
Lorenitz <lorena.g.saar@gmail.com>
2023-12-18 09:05:20 +0100

    adding flask request

--- chapter07/webapp/app.py
+++ chapter07/webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 import swimclub
 import os
 
@@ -62,9 +62,7 @@ def display_swimmer_files():
         data=session["swimmers"][name],
     )   
    
-app.run(debug=True)
-
-        
+           
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
