ad13f3582c4f223413ade4bf9553e68db454f05b
Lorenitz <lorena.g.saar@gmail.com>
2024-02-08 08:32:23 +0100

    Adding new files

--- chapter11/webapp/app.py
+++ chapter11/webapp/app.py
@@ -2,6 +2,7 @@ from flask import Flask, session, render_template, request
 import swimclub
 import os
 import data_utils
+import convert_utils
 
 app = Flask(__name__)
 app.secret_key = "You will never guess..."
@@ -29,7 +30,7 @@ def populate_data():
                 session["swimmers"][name] = []
             session["swimmers"][name].append(file)         
 
-
+#This @app line needs to use post as opposed to get, as the URL has data posted to id
 @app.post("/swimmers")
 def display_swimmers():
     #The line below does double-duty. It grabs the data sent from the HTML form, then saves it to Flask's session variable
@@ -39,7 +40,7 @@ def display_swimmers():
     return render_template(
         "select.html",
         title = "Select a swimmer",
-        url="/showfiles",
+        url="/showevents",
         select_id="swimmer",
         data=sorted(swimmers),        
     )
@@ -67,21 +68,61 @@ def display_swim_sessions():
 def get_swimmers_files(swimmer):
     populate_data()
     return str(session["swimmers"][swimmer])
+
+
    
-@app.post("/showfiles")
-def display_swimmer_files():
-    populate_data()
-    name = request.form["swimmer"]
-    return render_template (
-        "select.html",
-        title ="Select an event",
-        url="/showbarchart",
-        select_id="file",
-        data=session["swimmers"][name],
-    )   
+@app.post("/showevents")
+def display_swimmer_events():
+   session["swimmer"], session["age"] = request.form["swimmer"].split("-")
+    #grab the data you need from your database engine 
+    #The values of the placeholder variables are taken from Flask's "session" variable.
+   data = data_utils.get_swimmers_events(session["swimmer"], session["age"], session["chosen_date"])
+   
+   #Perform the necessary transformation
+   events = [f"{event[0]}{event[1]}" for event in data]
+   
+   #There are a few minor changes here to ensure the code refers to event and events instead of files
+   return render_template(
+       "select.html",
+       title="Select an event",
+       url="/showbarchart",
+       select_id="event",
+       data=events,
+   )
+   
+   
+   
 
 @app.post("/showbarchart")
 def show_bar_chart():
+    distance, stroke = request.form["event"].split("-")
+    data = data_utils.get_swimmers_times(
+        session["swimmer"],
+        session["age"],
+        distance,
+        stroke,
+        session["chosen_data"]
+    )
+    times = [time[0] for time in data]
+    
+    #perform the conversions
+    average_str, times_reversed, scaled = convert_utils.perform_conversions(times)
+    
+    #Grab the four world record times
+    world_records = convert_utils.get_wolrds(distance, stroke)
+    
+    #using a fstring to create a page header
+    header = f"{session['swimmer']}(Under {session['age']}) {distance} {stroke} - {session['chosen_date']}"
+    
+    return render_template(
+        "chart.html",
+        title=header,
+        data=list(zip(times_reversed, scaled)),
+        average=average_str,
+        worlds=world_records,
+        
+    )
+    
     file_id = request.form["file"]
     location = swimclub.produce_bar_chart(file_id, "templates/")
     return render_template(location.split("/")[-1])   
